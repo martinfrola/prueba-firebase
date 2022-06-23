4ccbdc51c30264ed7c6b93c899ec92a4c2df74c2
martinfrola <martinfrola@gmail.com>
2022-06-23 19:59:54 -0300

    primera prueba con Lukitaz

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { app } from "./index";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+import { sign } from "crypto";
 
+interface User {
+  name: string;
+  photo: string;
+  email: string;
+}
 function App() {
+  const [user, setUser] = useState<User>({
+    name: "",
+    photo: "",
+    email: "",
+  });
+
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+  const signIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        // The signed-in user info.
+        const userResult = result.user;
+        console.log(credential, result, user);
+        setUser({
+          name: userResult.displayName ?? "",
+          photo: userResult.photoURL ?? "",
+          email: userResult.email ?? "",
+        });
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+
+        console.log(error);
+
+        // ...
+      });
+  };
+
+  const verUsuario = () => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log(user);
+
+        // ...
+      } else {
+        console.log("No hay usuario");
+      }
+    });
+  };
+
+  const signOutUser = () => {
+    signOut(auth)
+      .then((user) =>
+        setUser({
+          name: "",
+          photo: "",
+          email: "",
+        })
+      )
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={signIn}>Traer info</button>
+      <h3>{user.name}</h3>
+      <p>{user.email}</p>
+      {user.photo && <img src={user.photo} alt="fotito del tincho " />}{" "}
+      <button onClick={verUsuario}>Ver usuario actual</button>
+      <button onClick={signOutUser}>LogOut</button>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBgH3YmojuveFxvam8M_vgiyhTCsgF1D04",
+  authDomain: "prueba-de-uso-73fa1.firebaseapp.com",
+  projectId: "prueba-de-uso-73fa1",
+  storageBucket: "prueba-de-uso-73fa1.appspot.com",
+  messagingSenderId: "1094786358640",
+  appId: "1:1094786358640:web:24f2f3457c1d28ae9883b5",
+  measurementId: "G-LMJRGXZEPG",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
